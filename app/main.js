ae2ff0236edf581943abcc85e0627431f7d8abfd
Anthony Gore <anthony@magnum.technology>
2017-08-18 16:13:23 +1000

    Changes

--- app/main.js
+++ app/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'vueify/lib/insert-css';
 
+Vue.config.productionTip = false;
+
 new Vue({
   el: '#app',
   render: h => h(App)
 })
-
-setTimeout(() => {
-  console.log('zzz');
-}, 1000);
