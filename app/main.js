3f7b5c1c75ebb328570c4921b2c6cdd800b0f8c2
Anthony Gore <anthony@magnum.technology>
2017-08-18 16:01:31 +1000

    Brunch setup

--- app/main.js
+++ app/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'vueify/lib/insert-css';
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+setTimeout(() => {
+  console.log('zzz');
+}, 1000);
---
+++ brunch-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: 'app.js'
+    }
+  }
+};
+
